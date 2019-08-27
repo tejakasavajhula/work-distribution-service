45b5a2f4548d79b80eef9969dd6a58b3de4c7764
tejakasavajhula <tejaswikvsk@gmail.com>
2019-08-27 11:16:44 -0400

    Add files via upload

---
+++ src/main/java/com/workdistribution/utils/ServiceConstants.java
@@ -0,0 +1,14 @@
+package com.workdistribution.utils;
+
+public class ServiceConstants {
+
+	public static final int HTTPSTATUS_OK_CODE = 200;
+	public static final String HTTPSTATUS_OK_VALUE = "Success";
+	public static final int HTTPSTATUS_BADREQUEST_CODE = 400;
+	public static final String HTTPSTATUS_BADREQUEST_VALUE = "Bad Request";
+	public static final int HTTPSTATUS_NOTFOUND_CODE = 404;
+	public static final String HTTPSTATUS_NOTFOUND_VALUE = "Not Found";
+	public static final int HTTPSTATUS_SERVICEUNAVAILABLE_CODE = 500;
+	public static final String HTTPSTATUS_SERVICEUNAVAILABLE_VALUE = "Service Unavailable";
+
+}
